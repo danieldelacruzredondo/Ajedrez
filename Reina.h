3112bdc2b8395084eb476c4bba11c498bdeb2791
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-04-23 20:03:33 +0200

    Create Reina.h

---
+++ Reina.h
@@ -0,0 +1,13 @@
+#include "Vector2D.h"
+class Reina
+{
+	Reina();
+	~Reina();
+	bool color; //0 si es blanca y 1 si es negra
+	Vector2D posicion;
+
+	void movimiento(float x, float y);
+	void comer(float x, float y, char);
+	void comido(float x, float y, char);
+	void dibuja();
+};
