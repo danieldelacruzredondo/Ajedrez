d89b202506b03551b69a5be00998d71da6edbc4e
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-04-23 20:02:23 +0200

    Create Caballo.h

---
+++ Caballo.h
@@ -0,0 +1,13 @@
+#include "Vector2D.h"
+class Caballo
+{
+	Caballo();
+	~Caballo();
+	bool color; //0 si es blanca y 1 si es negra
+	Vector2D posicion;
+
+	void movimiento(float x, float y);
+	void comer(float x, float y, char);
+	void comido(float x, float y, char);
+	void dibuja();
+};
