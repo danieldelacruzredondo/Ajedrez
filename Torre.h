3ffc8dd179b20d85599987cf5bbd18c627e0bf7c
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-04-24 18:24:45 +0200

    Update Torre.h

--- Torre.h
+++ Torre.h
@@ -1,12 +1,16 @@
-#include "Vector2D.h"
+#pragma once
+#include "VPosicion.h"
 class Torre
 {
 	Torre();
 	~Torre();
 	bool color; //0 si es blanca y 1 si es negra
-	Vector2D posicion;
+	VPosicion posicion;
+
+	VPosicion getPosicion();
 	void movimiento(float x, float y);
 	void comer(float x, float y);
 	void comido(float x, float y);
 	void dibuja();
+	
 };
