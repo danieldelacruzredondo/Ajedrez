e58b5d69ea5f34ec345341191a82a2242d8ef5e3
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-04-28 21:44:40 +0200

    Update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -1,8 +1,10 @@
 #pragma once
+#include"VPosicion.h"
 #include"Reina.h"
 #include"Rey.h"
 #include"Alfil.h"
-#include"VPosicion.h"
+#include"Caballo.h"
+#include"Torre.h"
 
 class Tablero
 {
@@ -11,6 +13,8 @@ public:
 	Reina reina;
 	Rey rey;
 	Alfil alfil11,alfil12,alfil21,alfil22;
+	Caballo caballo11, caballo12, caballo21, caballo22;
+	Torre torre11, torre12, torre21, torre22;
 
 	float x_ojo = 0;
 	float y_ojo = 0;
@@ -20,3 +24,4 @@ public:
 	void pintar();
 	void inicializar();
 };
+
