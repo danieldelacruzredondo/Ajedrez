7a25fd0bd3199d4d7ce2d27d31fb87e2a96fb979
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-04-28 20:16:04 +0200

    Update Rey.cpp

--- Rey.cpp
+++ Rey.cpp
@@ -1,10 +1,14 @@
-#include "Rey.h"
-#include "VPosicion.h"
+#include"Rey.h"
+#include"freeglut.h"
+#include"ETSIDI.h"
 
-Rey::Rey() {
-	posicion.x = 1;
-	posicion.y = 0;
-}
-VPosicion Rey::getPosicion() {
-	return posicion;
+ETSIDI::Sprite spritek1{ "bin/imagenes/rey_blancas.png", 9.17, 0.9, 11, 3.5 }, spritek2{ "bin/imagenes/rey_negras.png", 9.32, 14.9, 11, 3.5 };
+
+void Rey::dibuja()
+{
+	glPushMatrix();
+	glTranslatef(0, 0, 0.002);
+	spritek1.draw();
+	spritek2.draw();
+	glPopMatrix();
 }
