e1a0077170981b09fb90e748567bf80f023fd9fd
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-04-28 20:17:26 +0200

    Update Reina.cpp

--- Reina.cpp
+++ Reina.cpp
@@ -2,8 +2,7 @@
 #include"freeglut.h"
 #include"ETSIDI.h"
 
-//ETSIDI::SpriteSequence sprite{ "bin/imagenes/reina_blancas.png", 5 };
-ETSIDI:: Sprite spriteq1{ "bin/imagenes/reina_blancas.png", 7.05, 1, 11, 3.5}, spriteq2{ "bin/imagenes/reina_negras.png", 7.05, 15, 11, 3.5 };
+ETSIDI:: Sprite spriteq1{ "bin/imagenes/reina_blancas.png", 7.05, 1.1, 11, 3.5}, spriteq2{ "bin/imagenes/reina_negras.png", 7.05, 15.1, 11, 3.5 };
 
 void Reina::dibuja()
 {
