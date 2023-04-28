261f9f0c6d3a39eccccfd0b3d999bd5bd2825605
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-04-28 17:54:14 +0200

    Update Reina.cpp

--- Reina.cpp
+++ Reina.cpp
@@ -1,10 +1,15 @@
-#include "Reina.h"
-#include "VPosicion.h"
+#include"Reina.h"
+#include"freeglut.h"
+#include"ETSIDI.h"
 
-Reina::Reina() {
-	posicion.x = 1;
-	posicion.y = 0;
-}
-VPosicion Reina::getPosicion() {
-	return posicion;
+//ETSIDI::SpriteSequence sprite{ "bin/imagenes/reina_blancas.png", 5 };
+ETSIDI:: Sprite spriteq1{ "bin/imagenes/reina_blancas.png", 7.05, 1, 11, 3.5}, spriteq2{ "bin/imagenes/reina_negras.png", 7.05, 15, 11, 3.5 };
+
+void Reina::dibuja()
+{
+	glPushMatrix();
+	glTranslatef(0, 0, 0.002);
+	spriteq1.draw();
+	spriteq2.draw();
+	glPopMatrix();
 }
