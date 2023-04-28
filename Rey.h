5ab9cb0daaf4155ecfa3e6503ddf5a8f4ff5c955
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-04-28 20:16:46 +0200

    Update Rey.h

--- Rey.h
+++ Rey.h
@@ -1,17 +1,6 @@
 #pragma once
-#include "VPosicion.h"
 class Rey
 {
-	Rey();
-	~Rey();
-	bool color; //0 si es blanca y 1 si es negra
-	VPosicion posicion;
-
-	VPosicion getPosicion();
-	void movimiento(float x, float y);
-	void comer(float x, float y, char);
+public:
 	void dibuja();
-	void enroque(float x, float y);
-	void jaque(float x, float y);
-	bool jaqueMate();
 };
