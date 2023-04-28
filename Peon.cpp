d697bb21357a2c4f3d3d9fe9d06ad22c68621402
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-04-28 23:45:17 +0200

    Update Peon.cpp

--- Peon.cpp
+++ Peon.cpp
@@ -1,10 +1,19 @@
-#include "Peon.h"
-#include "VPosicion.h"
 
-Peon::Peon() {
-	posicion.x = 1;
-	posicion.y = 0;
+void Peon::setcolor(bool _color)
+{
+	color = _color;
 }
-VPosicion Peon::getPosicion() {
-	return posicion;
+
+void Peon::dibuja()
+{
+
+	if (color)
+		spritep = { "bin/imagenes/peon_negras.png", pos.x + 0.57f, pos.y - 0.1f, 11, 3.5 };
+	else
+		spritep = { "bin/imagenes/peon_blancas.png", pos.x + 0.67f, pos.y - 0.23f, 11, 3.5 };
+
+	glPushMatrix();
+	glTranslatef(0, 0, 0.002);
+	spritep.draw();
+	glPopMatrix();
 }
