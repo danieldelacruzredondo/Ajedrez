8da782da092b34f2bc2af46fa4df494b0ce7db93
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-04-28 21:42:58 +0200

    Update Torre.cpp

--- Torre.cpp
+++ Torre.cpp
@@ -1,10 +1,30 @@
-#include "Torre.h"
-#include "VPosicion.h"
+#include"Torre.h"
+#include"freeglut.h"
+#include"ETSIDI.h"
 
-Torre::Torre() {
-	posicion.x = 1;
-	posicion.y = 0;
+//ETSIDI::Sprite sprite{"bin/imagenes/alfil_negras.png", 5.35, 0.9, 11, 3.5};
+ETSIDI::Sprite spritet{ "bin/imagenes/torre_negras.png", 0.0, 0.0, 0.0, 0.0 };
+
+void Torre::setpos(VPosicion _pos)
+{
+	pos = _pos;
 }
-VPosicion Torre::getPosicion(){
-	return posicion;
+
+void Torre::setcolor(bool _color)
+{
+	color = _color;
+}
+
+void Torre::dibuja()
+{
+
+	if (color)
+		spritet = { "bin/imagenes/torre_negras.png", pos.x, pos.y - 0.1f, 11, 3.5 };
+	else
+		spritet = { "bin/imagenes/torre_blancas.png", pos.x, pos.y - 0.2f, 11, 3.5 };
+
+	glPushMatrix();
+	glTranslatef(0, 0, 0.002);
+	spritet.draw();
+	glPopMatrix();
 }
