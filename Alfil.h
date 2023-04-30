22f7105c457b729c57921ea6f12c6d259feaa941
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-04-30 20:18:23 +0200

    Update Alfil.h

--- Alfil.h
+++ Alfil.h
@@ -1,16 +1,18 @@
 #pragma once
 #include"VPosicion.h"
 
+
 class Alfil
 {
+	
 	VPosicion pos;
 	bool color;
-	Alfil alfil;
+	
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
-	VPosicion getpos();
+	VPosicion getpos() { return pos; };
 	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
