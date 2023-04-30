ba9883b8a95704da26820bdee44b82ca48295b7a
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 12:09:08 +0200

    Update Rey.h

--- Rey.h
+++ Rey.h
@@ -5,11 +5,13 @@ class Rey
 {
 	VPosicion pos;
 	bool color;
+	Rey rey;
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
 	VPosicion getpos();
+	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
 };
