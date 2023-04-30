f8e67ec63468ffe73ef5a937d8708095c253d045
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-04-30 20:23:55 +0200

    Update Reina.h

--- Reina.h
+++ Reina.h
@@ -1,17 +1,14 @@
-#pragma once
-#include"VPosicion.h"
-
 class Reina
 {
 	VPosicion pos;
 	bool color;
-	Reina reina;
+	
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
-	VPosicion getpos();
-        bool movimiento(VPosicion* fin);
-	
+	VPosicion getpos() { return pos; };
+	bool movimiento(VPosicion* fin);
+
 	friend class VMovimiento;
 };
