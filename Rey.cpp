165d2aeecb0d021fbad3fed66ca1bb598085a800
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 12:08:35 +0200

    Update Rey.cpp

--- Rey.cpp
+++ Rey.cpp
@@ -29,3 +29,37 @@ void Rey::dibuja()
 }
 
 VPosicion Rey::getpos() { return pos; }
+
+bool Rey::movimiento(VPosicion* fin) 
+{
+    int i, j;
+
+    rey.getpos();
+
+    //MOVIMIENTO DERECHA 
+    if (fin->x == pos.x + 2 && fin->y == pos.y /* && Ha detectado una pieza */) return true;
+
+    //MOVIMIENTO IZQUIERDA 
+    if (fin->x == pos.x - 2 && fin->y == pos.y /* && Ha detectado una pieza */) return true;
+
+    //MOVIMIENTO ARRIBA 
+    if (fin->x == pos.x && fin->y == pos.y + 2 /* && Ha detectado una pieza */) return true;
+   
+    //MOVIMIENTO ABAJO
+    if (fin->x == pos.x && fin->y == pos.y - 2 /* && Ha detectado una pieza */) return true;
+    
+    //MOVIMIENTO ARRIBA HACIA DERECHA 
+    if (fin->x == pos.x + 2 && fin->y == pos.y + 2 /* && Ha detectado una pieza */) return true;
+
+    //MOVIMIENTO ARRIBA HACIA IZQUIERDA 
+    if (fin->x == pos.x - 2 && fin->y == pos.y + 2 /* && Ha detectado una pieza */) return true;
+
+    //MOVIMIENTO ABAJO HACIA DERECHA 
+    if (fin->x == pos.x + 2 && fin->y == pos.y - 2 /* && Ha detectado una pieza */) return true;
+   
+    //MOVIMIENTO ABAJO HACIA IZQUIERDA  
+    if (fin->x == pos.x - 2 && fin->y == pos.y - 2 /* && Ha detectado una pieza */) return true;
+    
+    else return false;
+
+}
