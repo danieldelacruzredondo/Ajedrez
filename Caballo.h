045508c7c6c9d65bc01f6ad5afca13ac448a41a2
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 11:23:00 +0200

    Update Caballo.h

--- Caballo.h
+++ Caballo.h
@@ -5,11 +5,13 @@ class Caballo
 {
 	VPosicion pos;
 	bool color;
+	Caballo caballo;
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
 	VPosicion getpos();
-
+        bool movimiento(VPosicion* fin);
+	
 	friend class VMovimiento;
 };
