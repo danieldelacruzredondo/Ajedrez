6089497b22c975883c776e1a252cb1863193222f
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 11:11:48 +0200

    Update Torre.h

--- Torre.h
+++ Torre.h
@@ -5,11 +5,13 @@ class Torre
 {
 	VPosicion pos;
 	bool color;
+	Torre torre;
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
 	VPosicion getpos();
+	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
 };
