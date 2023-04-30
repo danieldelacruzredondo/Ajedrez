4696f4d42deeb09e9a9b383394288b0d1e401e14
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-04-30 20:25:02 +0200

    Update Reina.cpp

--- Reina.cpp
+++ Reina.cpp
@@ -7,35 +7,32 @@ ETSIDI::Sprite spriteq{ "bin/imagenes/reina_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
 void Reina::setpos(VPosicion _pos)
 {
-	pos = _pos;
+    pos = _pos;
 }
 
 void Reina::setcolor(bool _color)
 {
-	color = _color;
+    color = _color;
 }
 
 void Reina::dibuja()
 {
 
-	if (color)
-		spriteq = { "bin/imagenes/reina_negras.png", pos.x + 0.05f, pos.y + 0.075f , 11, 3.5 };
-	else
-		spriteq = { "bin/imagenes/reina_blancas.png", pos.x + 0.05f, pos.y + 0.12f , 11, 3.5 };
+    if (color)
+        spriteq = { "bin/imagenes/reina_negras.png", pos.x + 0.05f, pos.y + 0.075f , 11, 3.5 };
+    else
+        spriteq = { "bin/imagenes/reina_blancas.png", pos.x + 0.05f, pos.y + 0.12f , 11, 3.5 };
 
-	glPushMatrix();
-	glTranslatef(0, 0, 0.002);
-	spriteq.draw();
-	glPopMatrix();
+    glPushMatrix();
+    glTranslatef(0, 0, 0.002);
+    spriteq.draw();
+    glPopMatrix();
 }
 
-VPosicion Reina::getpos() { return pos; }
 
 bool Reina::movimiento(VPosicion* fin)
 {
     int i, j;
-
-    reina.getpos();
 
     //MOVIMIENTO DERECHA 
     if (pos.x < fin->x && pos.y == fin->y)
