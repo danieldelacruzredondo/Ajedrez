079a3c521f8474eddb2ecf2b2a4a59a1fc79a4fb
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 11:51:20 +0200

    Update Reina.h

--- Reina.h
+++ Reina.h
@@ -5,11 +5,13 @@ class Reina
 {
 	VPosicion pos;
 	bool color;
+	Reina reina;
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
 	VPosicion getpos();
-
+        bool movimiento(VPosicion* fin);
+	
 	friend class VMovimiento;
 };
