c4ddf7e69d1ccf76ac4ce87006b94b794475bd3d
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 11:10:09 +0200

    Update Peon.h

--- Peon.h
+++ Peon.h
@@ -5,11 +5,13 @@ class Peon
 {
 	VPosicion pos;
 	bool color;
+	Peon peon;
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
 	VPosicion getpos();
+	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
 };
