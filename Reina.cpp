18669c45926dfae157905dcd9530f32f5594b731
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 11:49:37 +0200

    Update Reina.cpp

--- Reina.cpp
+++ Reina.cpp
@@ -1,6 +1,7 @@
 #include"Reina.h"
 #include"freeglut.h"
 #include"ETSIDI.h"
+#include <math.h>
 
 ETSIDI::Sprite spriteq{ "bin/imagenes/reina_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
@@ -28,4 +29,96 @@ void Reina::dibuja()
 	glPopMatrix();
 }
 
-VPosicion Torre::getpos() { return pos; }
+VPosicion Reina::getpos() { return pos; }
+
+bool Reina::movimiento(VPosicion* fin)
+{
+    int i, j;
+
+    reina.getpos();
+
+    //MOVIMIENTO DERECHA 
+    if (pos.x < fin->x && pos.y == fin->y)
+    {
+        for (i = pos.x + 1; i <= fin->x; i++)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    //MOVIMIENTO IZQUIERDA  
+    if (pos.x > fin->x && pos.y == fin->y)
+    {
+        for (i = pos.x - 1; i >= fin->x; i--)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    //MOVIMIENTO ARRIBA  
+    if (pos.x == fin->x && pos.y < fin->y)
+    {
+        for (i = pos.y + 1; i <= fin->y; i++)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    //MOVIMIENTO ABAJO  
+    if (pos.x == fin->x && pos.y > fin->y)
+    {
+        for (i = pos.y - 1; i >= fin->y; i--)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    //Comprobar si el movimiento es diagonal
+    if (abs(fin->x - pos.x) != abs(fin->y - pos.y)) return false; //no es diagonal
+
+    //MOVIMIENTO DERECHA HACIA ARRIBA
+    if (pos.x < fin->x && pos.y < fin->y)
+    {
+        for (i = pos.x + 1, j = pos.y + 1; i <= fin->x && j <= fin->y; i++, j++)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    //MOVIMIENTO IZQUIERDA HACIA ARRIBA
+    if (pos.x > fin->x && pos.y < fin->y)
+    {
+        for (i = pos.x - 1, j = pos.y + 1; i >= fin->x && j <= fin->y; i--, j++)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    //MOVIMIENTO DERECHA HACIA ABAJO 
+    if (pos.x < fin->x && pos.y > fin->y)
+    {
+        for (i = pos.x + 1, j = pos.y - 1; i <= fin->x && j >= fin->y; i++, j--)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    //MOVIMIENTO IZQUIERDA HACIA ABAJO 
+    if (pos.x > fin->x && pos.y > fin->y)
+    {
+        for (i = pos.x - 1, j = pos.y - 1; i >= fin->x && j >= fin->y; i--, j--)
+        {
+            //Comprobar si la casilla está ocupada
+        }
+        return true;
+    }
+
+    else return false;
+}
