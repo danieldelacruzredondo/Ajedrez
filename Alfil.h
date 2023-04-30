d20cf592f5c26cae763e35de45b82b83c92949e9
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-04-30 11:34:46 +0200

    Update Alfil.h

--- Alfil.h
+++ Alfil.h
@@ -5,11 +5,13 @@ class Alfil
 {
 	VPosicion pos;
 	bool color;
+	Alfil alfil;
 public:
 	void dibuja();
 	void setpos(VPosicion _pos);
 	void setcolor(bool _color);
 	VPosicion getpos();
+	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
 };
