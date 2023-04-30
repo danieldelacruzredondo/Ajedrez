6623579343371486704366eea80e4f6fb2f566a8
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-04-30 20:26:44 +0200

    Update Torre.cpp

--- Torre.cpp
+++ Torre.cpp
@@ -5,38 +5,36 @@
 //ETSIDI::Sprite sprite{"bin/imagenes/alfil_negras.png", 5.35, 0.9, 11, 3.5};
 ETSIDI::Sprite spritet{ "bin/imagenes/torre_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
+
 void Torre::setpos(VPosicion _pos)
 {
-	pos = _pos;
+    pos = _pos;
 }
 
 void Torre::setcolor(bool _color)
 {
-	color = _color;
+    color = _color;
 }
 
 void Torre::dibuja()
 {
 
-	if (color)
-		spritet = { "bin/imagenes/torre_negras.png", pos.x, pos.y - 0.1f, 11, 3.5 };
-	else
-		spritet = { "bin/imagenes/torre_blancas.png", pos.x, pos.y - 0.2f, 11, 3.5 };
+    if (color)
+        spritet = { "bin/imagenes/torre_negras.png", pos.x, pos.y - 0.1f, 11, 3.5 };
+    else
+        spritet = { "bin/imagenes/torre_blancas.png", pos.x, pos.y - 0.2f, 11, 3.5 };
 
-	glPushMatrix();
-	glTranslatef(0, 0, 0.002);
-	spritet.draw();
-	glPopMatrix();
+    glPushMatrix();
+    glTranslatef(0, 0, 0.002);
+    spritet.draw();
+    glPopMatrix();
 }
 
-VPosicion Torre::getpos() { return pos; }
+
 
 bool Torre::movimiento(VPosicion* fin)
 {
     int i;
-
-    // Obtener posición pieza
-    torre.getpos();
 
     // MOVIMIENTO DERECHA
     if (pos.x < fin->x && pos.y == fin->y)
