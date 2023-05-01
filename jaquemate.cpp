950f7b08375c992e1437277c5cc33de67fc48ea2
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-01 12:12:14 +0200

    Update jaquemate.cpp

--- jaquemate.cpp
+++ jaquemate.cpp
@@ -7,6 +7,7 @@
 void OnDraw(void);
 void OnTimer(int value);
 void OnKeyboardDown(unsigned char key, int x, int y);
+void OnMouseClick(int button, int state, int x, int y);
 
 Tablero tablero;
 
@@ -36,6 +37,7 @@ int main(int argc, char* argv[])
 	glutDisplayFunc(OnDraw);
 	glutTimerFunc(25, OnTimer, 0);
 	glutKeyboardFunc(OnKeyboardDown);
+	glutMouseFunc(OnMouseClick);
 
 	glutMainLoop();
 	return 0;
@@ -53,9 +55,35 @@ void OnDraw(void)
 	glutSwapBuffers();
 }
 
+void OnMouseClick(int b, int state, int x, int y) 
+{
+	float _x = (float)(x - 115) / 42;
+	float _y = -(float)(y - 691) / 44;
+	float j = 0.0, i = 0.0;
+	bool ce = FALSE;
+	bool down = (state == GLUT_DOWN);
+	if (b == GLUT_LEFT_BUTTON && down)
+	{
+		for (j; j < 16.0f; j = j + 2.0f)
+		{
+			for (i; i < 16.0f; i = i + 2.0f)
+			{
+				if (_x < i + 2.0f && i < _x && _y < j + 2.0f && j < _y)
+				{
+					ce = TRUE;
+					break;
+				}
+			}
+			if (ce == TRUE)break;
+		}
+		tablero.casilla = VPosicion{ i + 1.0f, j + 1.0f };
+	}
+	glutPostRedisplay();
+}
+
 void OnKeyboardDown(unsigned char key, int x, int y)
 {
-	
+	//if(key=='f')tablero.reina0.setpos(VPosicion{ 7,5 });
 }
 
 void OnTimer(int value)
