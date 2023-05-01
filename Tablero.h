ac2ec762e3c8137fa1d52b19f1719af0b69fa043
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-01 15:44:00 +0200

    Update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -1,3 +1,4 @@
+#pragma once
 #include"VPosicion.h"
 #include"Reina.h"
 #include"Rey.h"
@@ -9,23 +10,24 @@
 
 class Tablero
 {
-	Reina reina0, reina1;
-	Rey rey0, rey1;
-	Alfil alfil01, alfil02, alfil11, alfil12;
-	Caballo caballo01, caballo02, caballo11, caballo12;
-	Torre torre01, torre02, torre11, torre12;
-	ListaPeones peones;
-	
-	public:
-
-	VPosicion casilla;
 	float x_ojo = 0;
 	float y_ojo = 0;
 	float z_ojo = 0;
 	int r, g, b;
+
+	Reina reina0,reina1;
+	Rey rey0,rey1;
+	Alfil alfil01,alfil02,alfil11,alfil12;
+	Caballo caballo01, caballo02, caballo11, caballo12;
+	Torre torre01, torre02, torre11, torre12;
+	ListaPeones peones;
+
+public:
+
+	VPosicion casilla;
+
 	Tablero();
 	void pintar();
 	void inicializar();
-	
+	void elegirficha(int b, int state, int x, int y);
 };
-
