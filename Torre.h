8b0cae6f33d236f22b02c8380b382d6a08695b25
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 13:24:25 +0200

    Update Torre.h

--- Torre.h
+++ Torre.h
@@ -5,9 +5,6 @@ class Torre: public Ficha
 {
 public:
 	void dibuja();
-	void setpos(VPosicion _pos) { pos = _pos; }
-	void setcolor(bool _color) { color = _color; }
-	VPosicion getpos() { return pos; }
 	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
