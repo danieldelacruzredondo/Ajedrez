e045e2dc1de8131e91ee7e75808be5648909c12f
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 13:22:42 +0200

    Update Caballo.h

--- Caballo.h
+++ Caballo.h
@@ -6,9 +6,6 @@ class Caballo:public Ficha
 	
 public:
 	void dibuja();
-	void setpos(VPosicion _pos) { pos = _pos; }
-	void setcolor(bool _color) { color = _color; }
-	VPosicion getpos() { return pos; }
 	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
