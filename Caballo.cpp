663aa2e26aab8987ad3b06dfd3f53bfe8d8e012c
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 10:48:31 +0200

    Update Caballo.cpp

--- Caballo.cpp
+++ Caballo.cpp
@@ -1,21 +1,17 @@
 #include"Caballo.h"
 #include"freeglut.h"
-#include"ETSIDI.h"
-
-//ETSIDI::Sprite sprite{"bin/imagenes/alfil_negras.png", 5.35, 0.9, 11, 3.5};
-ETSIDI::Sprite spritec{ "bin/imagenes/caballo_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
 void Caballo::dibuja()
 {
 
     if (color)
-        spritec = { "bin/imagenes/caballo_negras.png", pos.x , pos.y - 0.1f, 11, 3.5 };
+        sprite = { "bin/imagenes/caballo_negras.png", pos.x , pos.y - 0.1f, 11, 3.5 };
     else
-        spritec = { "bin/imagenes/caballo_blancas.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
+        sprite = { "bin/imagenes/caballo_blancas.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
 
     glPushMatrix();
     glTranslatef(0, 0, 0.002);
-    spritec.draw();
+    sprite.draw();
     glPopMatrix();
 }
 
