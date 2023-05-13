f790b051a41de2ebe949cab8f168851eb1ce28cd
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 10:08:54 +0200

    Update Rey.h

--- Rey.h
+++ Rey.h
@@ -1,16 +1,13 @@
 #pragma once
-#include"VPosicion.h"
+#include"Ficha.h"
 
-class Rey
+class Rey: public Ficha
 {
-	VPosicion pos;
-	bool color;
-	
 public:
 	void dibuja();
-	void setpos(VPosicion _pos);
-	void setcolor(bool _color);
-	VPosicion getpos() { return pos; };
+	void setpos(VPosicion _pos) { pos = _pos; }
+	void setcolor(bool _color) { color = _color; }
+	VPosicion getpos() { return pos; }
 	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
