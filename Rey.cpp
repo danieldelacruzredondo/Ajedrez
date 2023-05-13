c24ad2823c187b64b751f1eda41c7ed36188400a
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 10:49:39 +0200

    Update Rey.cpp

--- Rey.cpp
+++ Rey.cpp
@@ -1,20 +1,17 @@
 #include"Rey.h"
 #include"freeglut.h"
-#include"ETSIDI.h"
-
-ETSIDI::Sprite spritek{ "bin/imagenes/rey_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
 void Rey::dibuja()
 {
 
     if (color)
-        spritek = { "bin/imagenes/rey_negras.png", pos.x + 0.32f, pos.y - 0.1f, 11, 3.5 };
+        sprite = { "bin/imagenes/rey_negras.png", pos.x + 0.32f, pos.y - 0.1f, 11, 3.5 };
     else
-        spritek = { "bin/imagenes/rey_blancas.png", pos.x + 0.2f, pos.y - 0.12f, 11, 3.5 };
+        sprite = { "bin/imagenes/rey_blancas.png", pos.x + 0.2f, pos.y - 0.12f, 11, 3.5 };
 
     glPushMatrix();
     glTranslatef(0, 0, 0.002);
-    spritek.draw();
+    sprite.draw();
     glPopMatrix();
 }
 
