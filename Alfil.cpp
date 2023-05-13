32fcbbca072d76f618f867f64d93e6838ab5be63
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 10:47:25 +0200

    Update Alfil.cpp

--- Alfil.cpp
+++ Alfil.cpp
@@ -1,21 +1,17 @@
 #include"freeglut.h"
-#include"ETSIDI.h"
 #include <math.h>
-
-//ETSIDI_API Sprite(const char *texturePath, float x=0, float y=0, float width=-1, float height=-1);
-ETSIDI::Sprite spritea{ "bin/imagenes/alfil_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
 void Alfil::dibuja()
 {
 
     if (color)
-        sprite1 = { "bin/imagenes/alfil_negras.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
+        sprite = { "bin/imagenes/alfil_negras.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
     else
-        sprite1 = { "bin/imagenes/alfil_blancas.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
+        sprite = { "bin/imagenes/alfil_blancas.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
 
     glPushMatrix();
     glTranslatef(0, 0, 0.002);
-    sprite1.draw();
+    sprite.draw();
     glPopMatrix();
 }
 
