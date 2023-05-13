36d2a136bf4416a7273efac624d6f5fa157d0e84
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 10:49:18 +0200

    Update Reina.cpp

--- Reina.cpp
+++ Reina.cpp
@@ -1,21 +1,18 @@
 #include"Reina.h"
 #include"freeglut.h"
-#include"ETSIDI.h"
 #include <math.h>
-
-ETSIDI::Sprite spriteq{ "bin/imagenes/reina_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
 void Reina::dibuja()
 {
 
     if (color)
-        spriteq = { "bin/imagenes/reina_negras.png", pos.x + 0.05f, pos.y + 0.075f , 11, 3.5 };
+        sprite = { "bin/imagenes/reina_negras.png", pos.x + 0.05f, pos.y + 0.075f , 11, 3.5 };
     else
-        spriteq = { "bin/imagenes/reina_blancas.png", pos.x + 0.05f, pos.y + 0.12f , 11, 3.5 };
+        sprite = { "bin/imagenes/reina_blancas.png", pos.x + 0.05f, pos.y + 0.12f , 11, 3.5 };
 
     glPushMatrix();
     glTranslatef(0, 0, 0.002);
-    spriteq.draw();
+    sprite.draw();
     glPopMatrix();
 }
 
