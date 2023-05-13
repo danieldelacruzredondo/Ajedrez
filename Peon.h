12f237e73e34cc40421d059226481b38d39eead7
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 10:07:40 +0200

    Update Peon.h

--- Peon.h
+++ Peon.h
@@ -5,9 +5,9 @@ class Peon:public Ficha
 {
 public:
 	void dibuja();
-	void setpos(VPosicion _pos);
-	void setcolor(bool _color);
-	VPosicion getpos() { return pos; };
+	void setpos(VPosicion _pos) { pos = _pos; }
+	void setcolor(bool _color) { color = _color; }
+	VPosicion getpos() { return pos; }
 	bool movimiento(VPosicion* fin);
 
 	friend class VMovimiento;
