3e9820180ff11f095e2ebb1b9e15af5409e2c824
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-13 13:23:22 +0200

    Update Ficha.h

--- Ficha.h
+++ Ficha.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "VPosicion.h"
 #include "ETSIDI.h"
-#define numFichas=32
 
 //ETSIDI_API Sprite(const char *texturePath, float x=0, float y=0, float width=-1, float height=-1);
 
@@ -13,8 +12,12 @@ protected:
 	ETSIDI::Sprite sprite{ "bin/imagenes/rey_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
 public:
-	virtual ~Ficha() {};
-	virtual void mueve();
+	virtual ~Ficha() {}
+	virtual void dibuja(){}
+	virtual void mueve(){}
+	void setpos(VPosicion _pos) { pos = _pos; }
+	void setcolor(bool _color) { color = _color; }
+	VPosicion getpos() { return pos; }
+	bool getcolor() { return color; }
 	virtual void destruyeficha();
-	
 };
