ff128202bc64017e01e579c55e11fcb12f8d163b
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-16 19:06:16 +0200

    Update ListaFichas.h

--- ListaFichas.h
+++ ListaFichas.h
@@ -8,9 +8,15 @@ class ListaFichas
 {
 	Ficha* listafichas[NumMax]{};
 	int numero = 0;
+	int casillafi;
+	bool turno = 0;
+	
 public:
 	void agregar(Ficha* _ficha);
 	void inicializa();
 	void dibuja();
 	int getnumero() { return numero; }
+	int Comprobar_Posicion(VPosicion cas);
+	bool Comprobar_Color(int n) { return listafichas[n]->getcolor(); }
+	void ElegirFicha(VPosicion cas);
 };
