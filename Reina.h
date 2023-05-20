6e0a0b74149e928775a40fd0613f0c03549e2b2e
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-05-20 18:21:33 +0200

    Update Reina.h

--- Reina.h
+++ Reina.h
@@ -1,11 +1,11 @@
 #pragma once
-#include "Ficha.h"
-#include "ListaFichas.h"
+#include"Ficha.h"
 
-class Reina : public Ficha
+class Rey : public Ficha
 {
 public:
 	void dibuja();
 	bool mueve(VPosicion fin);
 
+	friend class VMovimiento;
 };
