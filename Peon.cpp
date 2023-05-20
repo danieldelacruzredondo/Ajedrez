758e951dd5f210966ef8a56fb3916e2d825ce0d4
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-20 08:20:03 +0200

    Update Peon.cpp

--- Peon.cpp
+++ Peon.cpp
@@ -14,67 +14,86 @@ void Peon::dibuja()
 	glPopMatrix();
 }
 
-bool Peon::movimiento(VPosicion* fin)
+bool Peon::mueve(VPosicion fin)
 {
+
 	// los peones blancos solo se mueven hacia arriba
 	if (color == 0)
 	{
-		// MOVIMIENTO 1: máximo 2 casillas hacia delante 
-		if (pos.y == 3 && (fin.y == pos.y + 2 || fin.y == pos.y + 4) && fin.x == pos.x)
+		// SOLO SE VA A MOVER EL PEON.
+		if (ListaFichas::Comprobar_Posicion(VPosicion{ fin }) == 32)
 		{
-			//comprobar que el peón no se salta ninguna pieza - cuando se mueve 2 casillas hacia delante
-			if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y + 2) }) < 32)return false;
-			//Movimiento del peon si se ha elegido mover 2 casillas.
-			if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y + 4) }) == 32 && fin == VPosicion{ pos.x,pos.y + 4 })
+			// MOVIMIENTO 1: máximo 2 casillas hacia delante 
+			if (pos.y == 3 && (fin.y == pos.y + 2 || fin.y == pos.y + 4) && fin.x == pos.x)
 			{
+				//comprobar que el peón no se salta ninguna pieza - cuando se mueve 2 casillas hacia delante
+				if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y + 2) }) < 32)return false;
+				//Movimiento del peon si se ha elegido mover 2 casillas.
+				if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y + 4) }) == 32 && fin == VPosicion{ pos.x,pos.y + 4 })
+				{
+					pos = fin;
+					return true;
+				}
+			}
+			// Movimientos del peon si se ha elegido mover una casilla.
+			if (fin.y == pos.y + 2 && fin.x == pos.x)
+			{
+				if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y + 2) }) < 32)return false;
+				pos = fin;
+				return true;
+			}
+			else return false;
+		}
+
+		// FICHA BLANCA COME FICHA NEGRA EN DIAGONAL.
+		if (fin.y == (pos.y + 2) && (fin.x == (pos.x + 2) || fin.x == (pos.x - 2)) && ListaFichas::Comprobar_Posicion(fin) < 32)
+		{
+			if (ListaFichas::Comprobar_Color(ListaFichas::Comprobar_Posicion(fin)) == true)
+			{
+				ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
 				pos = fin;
 				return true;
 			}
 		}
-		// Movimientos del peon si se ha elegido mover una casilla.
-		if (fin.y == pos.y + 2 && fin.x == pos.x)
-		{
-			if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y + 2) }) < 32)return false;
-			pos = fin;
-			return true;
-		}
 		else return false;
-
-		// COMER PIEZAS NEGRAS: en diagonal un único desplazamiento
-		//if (fin.y == pos.y + 2 && (fin.x == pos.x + 2 || fin.x == pos.x - 2) && ListaFichas::Comprobar_Posicion(VPosicion{ pos.x + 2,(pos.y + 2) })<32 && ListaFichas::Comprobar_Posicion(VPosicion{ pos.x - 2,(pos.y + 2) }) < 32)
-		{
-			//return true;
-		}
 	}
 	//Los peones negros solo se mueven hacia abajo 
 	else
 	{
-		// MOVIMIENTO 1: máximo 2 casillas hacia delante 
-		if (pos.y == 13 && (fin.y == pos.y - 2 || fin.y == pos.y - 4) && fin.x == pos.x)
+		// SOLO SE VA A MOVER EL PEON.
+		if (ListaFichas::Comprobar_Posicion(VPosicion{ fin }) == 32)
 		{
-			//comprobar que el peón no se salta ninguna pieza - cuando se mueve 2 casillas hacia delante
-			if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y - 2) }) < 32)return false;
-			// Movimiento del peon si se ha elegido mover 2 casillas.
-			if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y - 4) }) == 32 && VPosicion{ pos.x,pos.y - 4 } == fin)
+			// MOVIMIENTO 1: máximo 2 casillas hacia delante 
+			if (pos.y == 13 && (fin.y == pos.y - 2 || fin.y == pos.y - 4) && fin.x == pos.x)
 			{
+				//comprobar que el peón no se salta ninguna pieza - cuando se mueve 2 casillas hacia delante
+				if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y - 2) }) < 32)return false;
+				// Movimiento del peon si se ha elegido mover 2 casillas.
+				if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y - 4) }) == 32 && VPosicion{ pos.x,pos.y - 4 } == fin)
+				{
+					pos = fin;
+					return true;
+				}
+			}
+			// Movimiento del peon si se ha elegido mover una casilla.
+			if (fin.y == pos.y - 2 && fin.x == pos.x)
+			{
+				if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y - 2) }) < 32)return false;
+				pos = fin;
+				return true;
+			}
+			else return false;
+		}
+		// FICHA NEGRA COME FICHA BLANCA EN DIAGONAL.
+		if (fin.y == (pos.y - 2) && (fin.x == (pos.x + 2) || fin.x == (pos.x - 2)) && ListaFichas::Comprobar_Posicion(fin) < 32)
+		{
+			if (ListaFichas::Comprobar_Color(ListaFichas::Comprobar_Posicion(fin)) == false)
+			{
+				ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
 				pos = fin;
 				return true;
 			}
 		}
-		// Movimiento del peon si se ha elegido mover una casilla.
-		if (fin.y == pos.y - 2 && fin.x == pos.x)
-		{
-			if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x,(pos.y - 2) }) < 32)return false;
-			pos = fin;
-			return true;
-		}
 		else return false;
-
-		// COMER PIEZAS NEGRAS: en diagonal un único desplazamiento
-		//if (fin.y == pos.y - 2 && (fin.x == pos.x + 2 || fin.x == pos.x - 2)/* &&  Ha detectado una pieza negra */)
-		{
-			//return true;
-		}
 	}
-
 }
