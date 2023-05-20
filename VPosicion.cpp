947d151b7b05e50ac7eff675145df893b2932eed
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-20 13:33:25 +0200

    Update VPosicion.cpp

--- VPosicion.cpp
+++ VPosicion.cpp
@@ -5,3 +5,9 @@ bool VPosicion::operator==(VPosicion b)
 	if (b.x == x && b.y == y)return true;
 	else return false;
 }
+
+bool VPosicion::operator!=(VPosicion b)
+{
+	if (b.x != x || b.y != y)return true;
+	else return false;
+}
