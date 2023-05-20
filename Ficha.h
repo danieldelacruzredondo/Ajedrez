849e0a20d8295e6ce9d72929edda7c711220df12
Kike <47853580+enriquehernandezmartinez@users.noreply.github.com>
2023-05-20 08:11:00 +0200

    Update Ficha.h

--- Ficha.h
+++ Ficha.h
@@ -8,18 +8,20 @@ class Ficha
 {
 protected:
 	VPosicion pos;
-	
+	bool vida = true;
 	bool color;
 	ETSIDI::Sprite sprite{ "bin/imagenes/rey_negras.png", 0.0, 0.0, 0.0, 0.0 };
 
 public:
 	virtual ~Ficha() {}
+	virtual void inicializa(VPosicion _pos, bool _color) { pos = _pos, color = _color; }
 	virtual void dibuja() {}
 	virtual bool mueve(VPosicion) { return false; }
-	virtual void inicializa(VPosicion _pos, bool _color) { pos = _pos, color = _color; }
 	void setpos(VPosicion _pos) { pos = _pos; }
 	void setcolor(bool _color) { color = _color; }
 	VPosicion getpos() { return pos; }
 	bool getcolor() { return color; }
-	virtual void destruyeficha();
+	void morir();
+	void setmorir() { vida = false; }
+	bool getvida() { return vida; }
 };
