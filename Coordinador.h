ea48b79754591bf415d588153a8171af8a5253c2
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-05-20 14:34:14 +0200

    Create Coordinador.h

---
+++ Coordinador.h
@@ -0,0 +1,17 @@
+#include "Tablero.h"
+#include "ETSIDI.h"
+
+class Coordinador
+{
+public:
+	Coordinador();
+	virtual ~Coordinador() {};
+
+	void Elegir_modo_juego(int b, int state, int x, int y);
+	void dibujar();
+
+protected:
+	Tablero tablero;
+	enum Estado{INICIO, JUEGO};
+	Estado estado;
+};
