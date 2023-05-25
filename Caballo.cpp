45a3521344dcad42f53fc9a0293922fc6552bc9e
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-05-25 12:32:16 +0200

    Update Caballo.cpp

--- Caballo.cpp
+++ Caballo.cpp
@@ -4,15 +4,27 @@
 void Caballo::dibuja()
 {
 
-	if (color)
-		sprite = { "bin/imagenes/caballo_negras.png", pos.x , pos.y - 0.1f, 11, 3.5 };
-	else
-		sprite = { "bin/imagenes/caballo_blancas.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
+    if (color)
+        sprite = { "bin/imagenes/caballo_negras.png", pos.x , pos.y - 0.1f, 11, 3.5 };
+    else
+        sprite = { "bin/imagenes/caballo_blancas.png", pos.x + 0.35f, pos.y - 0.1f, 11, 3.5 };
 
-	glPushMatrix();
-	glTranslatef(0, 0, 0.002);
-	sprite.draw();
-	glPopMatrix();
+    glPushMatrix();
+    glTranslatef(0, 0, 0.002);
+    sprite.draw();
+    glPopMatrix();
+
+    if (pintar) {
+        glPushMatrix();
+        glColor3ub(25, 150, 100);
+        glBegin(GL_POLYGON);
+        glVertex3f(pos.x+1,pos.y+1, 0.001f);
+        glVertex3f(pos.x+1, pos.y-1, 0.001f);
+        glVertex3f(pos.x-1, pos.y-1, 0.001f);
+        glVertex3f(pos.x-1, pos.y+1, 0.001f);
+        glEnd();
+        glPopMatrix();
+    }
 }
 
 bool Caballo::mueve(VPosicion fin)
@@ -33,9 +45,10 @@ bool Caballo::mueve(VPosicion fin)
     }
     // SI SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x + 4,pos.y + 2 }) == 32) && (fin == (VPosicion{ pos.x + 4,pos.y + 2 })))
-     {
-            pos = fin;
-            return true;
+    {
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
 
     // MOVIMIENTO DERECHA 1 ARRIBA 2
@@ -46,16 +59,18 @@ bool Caballo::mueve(VPosicion fin)
         {
             ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
             pos = fin;
+            pintar -= 1;
             return true;
         }
     }
     // SI SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x + 2,pos.y + 4 }) == 32) && (fin == (VPosicion{ pos.x + 2,pos.y + 4 })))
     {
-            pos = fin;
-            return true;
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
-  
+
     // MOVIMIENTO DERECHA 2 ABAJO 1
     // COMPROBAMOS SI COME PIEZA
     if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x + 4 ,pos.y - 2 }) < 32 && VPosicion{ pos.x + 4, pos.y - 2 } == fin)
@@ -64,14 +79,16 @@ bool Caballo::mueve(VPosicion fin)
         {
             ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
             pos = fin;
+            pintar -= 1;
             return true;
         }
     }
     // SI SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x + 4,pos.y - 2 }) == 32) && (fin == (VPosicion{ pos.x + 4,pos.y - 2 })))
     {
-            pos = fin;
-            return true;
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
 
     //MOVIMIENTO DERECHA 1 ABAJO 2
@@ -82,16 +99,18 @@ bool Caballo::mueve(VPosicion fin)
         {
             ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
             pos = fin;
+            pintar -= 1;
             return true;
         }
     }
     // SI SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x + 2,pos.y - 4 }) == 32) && (fin == (VPosicion{ pos.x + 2,pos.y - 4 })))
     {
-            pos = fin;
-            return true;
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
-    
+
     //MOVIMIENTO IZQUIERDA 1 ARRIBA 2
      // COMPROBAMOS SI COME PIEZA
     if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x - 2 ,pos.y + 4 }) < 32 && VPosicion{ pos.x - 2, pos.y + 4 } == fin)
@@ -100,14 +119,16 @@ bool Caballo::mueve(VPosicion fin)
         {
             ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
             pos = fin;
+            pintar -= 1;
             return true;
         }
     }
     // SI SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x - 2,pos.y + 4 }) == 32) && (fin == (VPosicion{ pos.x - 2,pos.y + 4 })))
     {
-            pos = fin;
-            return true;
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
 
     //MOVIMIENTO IZQUIERDA 2 ARRIBA 1  
@@ -118,14 +139,16 @@ bool Caballo::mueve(VPosicion fin)
         {
             ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
             pos = fin;
+            pintar -= 1;
             return true;
         }
     }
     // SI SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x - 4,pos.y + 2 }) == 32) && (fin == (VPosicion{ pos.x - 4,pos.y + 2 })))
     {
-            pos = fin;
-            return true;
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
 
     // MOVIMIENTO IZQUIERDA 1 ABAJO 2 
@@ -136,16 +159,18 @@ bool Caballo::mueve(VPosicion fin)
         {
             ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
             pos = fin;
+            pintar -= 1;
             return true;
         }
     }
     // SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x - 2,pos.y - 4 }) == 32) && (fin == (VPosicion{ pos.x - 2,pos.y - 4 })))
     {
-            pos = fin;
-            return true;
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
-   
+
     // MOVIMIENTO IZQUIERDA 2 ABAJO 1 
     // COMPROBAMOS SI COME PIEZA
     if (ListaFichas::Comprobar_Posicion(VPosicion{ pos.x - 4 ,pos.y - 2 }) < 32 && VPosicion{ pos.x - 4, pos.y - 2 } == fin)
@@ -154,14 +179,16 @@ bool Caballo::mueve(VPosicion fin)
         {
             ListaFichas::Matar(ListaFichas::Comprobar_Posicion(fin));
             pos = fin;
+            pintar -= 1;
             return true;
         }
     }
     // SOLO SE MUEVE
     if ((ListaFichas::Comprobar_Posicion(VPosicion{ pos.x - 4,pos.y - 2 }) == 32) && (fin == (VPosicion{ pos.x - 4,pos.y - 2 })))
     {
-            pos = fin;
-            return true;
+        pos = fin;
+        pintar -= 1;
+        return true;
     }
 
     return false;
